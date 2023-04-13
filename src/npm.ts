c628961df0bc4a075406029124753a69a0d13d20
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-12 17:08:50 -0700

    prefer pnpm

--- src/npm.ts
+++ src/npm.ts
@@ -20,18 +20,18 @@ export function whichPm(cwd = '.'): PackageManager {
     }
     const userAgent = process.env.npm_config_user_agent;
     if (userAgent) {
-      if (userAgent.startsWith('yarn')) {
-        return 'yarn';
-      } else if (userAgent.startsWith('pnpm')) {
+      if (userAgent.startsWith('pnpm')) {
         return 'pnpm';
+      } else if (userAgent.startsWith('yarn')) {
+        return 'yarn';
       }
     }
     try {
-      execSync('yarn --version', { stdio: 'ignore' });
-      return 'yarn';
-    } catch {
       execSync('pnpm --version', { stdio: 'ignore' });
       return 'pnpm';
+    } catch {
+      execSync('yarn --version', { stdio: 'ignore' });
+      return 'yarn';
     }
   } catch {
     return 'npm';
